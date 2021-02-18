b7a600e13c9f53df41014413b9c3212bade7a23b
omeralmali1 <omeralmali1@gmail.com>
2021-02-18 22:34:44 +0300

    Bug Fixed.

--- StudentExamNote/Program.cs
+++ StudentExamNote/Program.cs
@@ -6,19 +6,19 @@ namespace StudentExamNote
     {
         static void Main(string[] args)
         {
-            int vize, final, ort;
-            Console.Write("Vize notunu giriniz: ");
-            vize = Convert.ToInt32(Console.ReadLine());
+            int exam1, final, ava;
+            Console.Write("Please Enter First Exam Note: ");
+            exam1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Final notunu giriniz: ");
             final = Convert.ToInt32(Console.ReadLine());
-            ort = Convert.ToInt32(vize * 0.4 + final * 0.6);
+            ava = Convert.ToInt32(exam1 * 0.4 + final * 0.6);
             Console.WriteLine();
-            Console.WriteLine("Ortalamanız = {0}", ort);
+            Console.WriteLine("Ortalamanız = {0}", ava);
             Console.WriteLine();
-            if (ort < 50 || final < 50)
-                Console.WriteLine("Kaldınız.");
+            if (ava < 50 || final < 50)
+                Console.WriteLine("You Fail.");
             else
-                Console.WriteLine("Geçtiniz.");
+                Console.WriteLine("You Passed.");
 
             Console.ReadKey();
         }
