da61a27645132565c80ec614d97790a561009e30
omerfdev <omerfdev@gmail.com>
2023-05-13 16:08:13 +0300

    Commit

--- StudentExamNote/Program.cs
+++ StudentExamNote/Program.cs
@@ -6,41 +6,10 @@ namespace StudentExamNote
     {
         static void Main(string[] args)
         {
+            Student student = new Student();
 
-            double average=PointCalculate();
-            average= Math.Round(average); 
-
-            Console.WriteLine("Average = {0}", average);
-            if (average < 50 )
-                Console.WriteLine("You Failed");
-            else
-                Console.WriteLine("You Passed.");
-
-            Console.ReadKey();
+            Console.ReadLine();
         }
-
-        private static double PointCalculate()
-        {
-            Console.WriteLine("\t" + "{0:r}", DateTime.Now);
-            double firstExam = 0; double final = 0; double average = 0;
-
-            try
-            {
-                Console.Write("Please Enter First Exam Note: ");
-                firstExam = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please Enter Final Note: ");
-                final = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (FormatException ex)
-            {
-                Console.Clear();
-                PointCalculate();
-                Console.WriteLine("Wrong Format Type" + ex.Message);
-            }
-            finally { average = (firstExam * 0.4) + (final * 0.6); }
-                return average; 
-
-        }
-
+                
     }
 }
---
+++ StudentExamNote/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace StudentExamNote
+{
+    internal class Student
+    {
+        public Student()
+        {
+            Console.WriteLine("{0:r}", DateTime.Now);
+            Console.Write("Please Enter First Exam Note: ");
+            StudentExam = double.Parse(Console.ReadLine());
+            Console.Write("Please Enter Final Note: ");
+            StudentExamFinal = Convert.ToInt32(Console.ReadLine());
+            Calculate(); 
+        }
+        public double StudentExam { get; set; }
+        public double StudentExamFinal { get; set; }
+        public double StudentPoint { get; set; }
+        /// <summary>
+        /// The method that takes the average exam
+        /// </summary>
+        /// <returns></returns>
+        public void Calculate()
+        {
+            StudentPoint = (StudentExam * 0.4 + StudentExamFinal * 0.6);
+            if (StudentPoint < 50)
+              Console.WriteLine($"You Failed... Your Point:{StudentPoint}");
+            else
+                Console.WriteLine($"You Passed... Your Point:{StudentPoint}");
+
+        }
+    }
+}
